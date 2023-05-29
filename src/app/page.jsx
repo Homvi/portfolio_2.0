ab3fc94eb8bbd39fc53eba1c904f508a65ef010e
homvi <honvedoadamkj@gmail.com>
2023-05-29 17:02:41 +0200

    ad nunito font

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -41,13 +41,13 @@ export default function Home() {
       <Navbar isBorder={isBorderOnNav} />
       <MobileNavbar />
       {/* landing section */}
-      <section className="min-h-screen bg-gradient-to-br font-extralight from-[#071927]  to-[#181818] flex flex-col justify-center items-center">
+      <section className="min-h-screen font-nunito bg-gradient-to-br font-extralight from-[#071927]  to-[#181818] flex flex-col justify-center items-center">
         <div className="text-4xl xl:text-6xl w-[95%]  flex justify-around  text-[#27465A] mb-5 md:mb-16 ">
           {arrayOfLettersOfName.map((letter, id) => {
             return <span key={id}>{letter}</span>;
           })}
         </div>
-        <div className="w-[70%] text-2xl xl:text-4xl text-[#515151] md:w-[50%] flex justify-around">
+        <div className="w-[70%] text-2xl  xl:text-4xl text-[#515151] md:w-[50%] flex justify-around">
           {arrayOfLettersOfTitle.map((letter, id) => {
             return <span key={id}>{letter}</span>;
           })}
