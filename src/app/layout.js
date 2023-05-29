1ca02a227fdde91050833651ebe67c120df66bee
homvi <honvedoadamkj@gmail.com>
2023-05-29 15:40:05 +0200

    change body background

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,11 +8,12 @@ export const metadata = {
   description:
     "Hi, I'm Adam!-Frontend development isn't just my profession—it's my playground. I thrive in turning creative concepts into live, user-friendly applications.Ready to rock the digital world with my code?",
 };
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>{children}</body>
+      <body suppressHydrationWarning={true} className={`${inter.className}`}>
+        {children}
+      </body>
     </html>
   );
 }
