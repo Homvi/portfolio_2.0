d98d9718fc60df2b4ce2f55120edf5c38c60d8b1
homvi <honvedoadamkj@gmail.com>
2023-05-29 11:46:52 +0200

    add logos

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,10 +1,14 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import inkedin from "../../assets/linkedin.svg";
+import github from "../../assets/github.svg";
+import CVLogo from "../../assets/CVLogo.svg";
 
 const Navbar = () => {
   return (
-    <nav className="fixed flex justify-between w-full p-3  border-b-[1px] border-[#27465A] text-md">
-      <div className="flex gap-8 px-3 text-[#F2F2F2] font-extralight">
+    <nav className="fixed flex justify-between w-full py-3 px-8  border-b-[1px] backdrop-blur-md border-[#27465a91] text-md">
+      <div className="flex gap-8  text-[#F2F2F2] font-extralight">
         <Link className="opacity-80 hover:opacity-100" href={"#about"}>
           About
         </Link>
@@ -15,10 +19,28 @@ const Navbar = () => {
           Contact
         </Link>
       </div>
-      <div className="flex gap-5">
-        <Link href={"#about"}>About</Link>
-        <Link href={"#portfolio"}>Portfolio</Link>
-        <Link href={"#contact"}>Contact</Link>
+      <div className="flex gap-10">
+        <Link href={"#about"}>
+          <Image
+            className="opacity-60 hover:opacity-100"
+            src={inkedin}
+            alt="linkedin"
+          />
+        </Link>
+        <Link href={"#portfolio"}>
+          <Image
+            className="opacity-80 hover:opacity-100"
+            src={github}
+            alt="linkedin"
+          />
+        </Link>
+        <Link href={"#contact"}>
+          <Image
+            className="opacity-95 hover:opacity-100"
+            src={CVLogo}
+            alt="linkedin"
+          />
+        </Link>
       </div>
     </nav>
   );
