7b967a4229f4461f14e587ba68b9e526a37d42c1
homvi <honvedoadamkj@gmail.com>
2023-05-30 12:10:37 +0200

    add animations to intro

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -75,13 +75,27 @@ export default function Home() {
         className="relative z-10 text-[#F2F2F2] flex flex-col md:justify-center h-screen p-5 md:p-10 "
       >
         <div className="md:w-[60%] text-md min-h-[60%] flex flex-col items-start justify-center sm:text-lg">
-          <h3 className="text-xl sm:text-2xl">Hi, I&apos;m Adam!</h3>
-          <p className="py-10">
+          <h3
+            data-aos="fade-right"
+            data-aos-duration="300"
+            data-aos-delay="200"
+            className="text-xl sm:text-2xl"
+          >
+            Hi, I&apos;m Adam!
+          </h3>
+          <p
+            data-aos="fade-right"
+            data-aos-duration="300"
+            data-aos-delay="400"
+            className="py-10"
+          >
             Frontend development isn&apos;t just my profession — it&apos;s my
             playground. <br /> I thrive in turning creative concepts into live,
             user-friendly applications.
           </p>
-          <p>Ready to rock the digital world with my code?</p>
+          <p data-aos="fade-right" data-aos-duration="300" data-aos-delay="600">
+            Ready to rock the digital world with my code?
+          </p>
         </div>
         <div className="absolute bottom-0 right-0 w-[300px] sm:w-[500px]">
           <Image height={1524} width={1524} src={profile} alt="adam" />
