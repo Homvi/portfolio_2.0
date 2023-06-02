816b551551cf04698ea0181ba7ec486bbac567b2
homvi <honvedoadamkj@gmail.com>
2023-06-02 14:49:21 +0200

    add header to portfolio

--- src/app/sections/Portfolio.jsx
+++ src/app/sections/Portfolio.jsx
@@ -4,9 +4,11 @@ const Portfolio = () => {
   return (
     <section
       id="portfolio"
-      className="relative z-10 text-[#F2F2F2] flex flex-col md:justify-center h-screen p-5 md:p-10 "
+      className="relative z-10 text-[#F2F2F2]  h-screen p-5 md:p-10 "
     >
-        
+      <h2 className="mt-10 text-4xl tracking-[20px] font-nunitoXLight text-center sm:text-left">
+        Portfolio
+      </h2>
     </section>
   );
 };
