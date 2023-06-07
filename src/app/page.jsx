ce846edc23138f6dcdf6463f663cde7309c93192
homvi <honvedoadamkj@gmail.com>
2023-06-07 09:55:48 +0200

    comment

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,28 +1,34 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Navbar from "./components/Navbar";
-import "aos/dist/aos.css";
-import MobileNavbar from "./components/MobileNavbar";
 import Head from "next/head";
-
+// Importing components from local project files
+import Navbar from "./components/Navbar";
+import MobileNavbar from "./components/MobileNavbar";
+// Import the CSS for the "aos" (Animate on Scroll) library
+import "aos/dist/aos.css";
+// Importing section components from local project files
 import Landing from "./sections/Landing";
 import Intro from "./sections/Intro";
 import Contact from "./sections/Contact";
 import Portfolio from "./sections/Portfolio";
 
 export default function Home() {
+  // Using the useState hook to create a state variable for managing the border of the Navbar
   const [isBorderOnNav, setIsBorderOnNav] = useState(false);
 
+  // Using the useEffect hook to add an event listener to the scroll event when the component mounts
   useEffect(() => {
     window.addEventListener("scroll", (event) => {
+      // If the page has been scrolled (window.pageYOffset is non-zero), set isBorderOnNav to true
       if (window.pageYOffset) {
         setIsBorderOnNav(true);
       } else {
+        // Otherwise, set it to false
         setIsBorderOnNav(false);
       }
     });
-  }, []);
+  }, []);// Empty dependency array means this effect will only run once, on mount
 
   return (
     <>
